1bd0a3a7a6f1356d43d35cfcb412b0b612e2cead
Stephan Lachnit <stephanlachnit@debian.org>
2025-08-26 17:42:14 +0200

    implement demo

--- src/TempMon/TempMon.py
+++ src/TempMon/TempMon.py
@@ -3,8 +3,22 @@ SPDX-FileCopyrightText: 2025 DESY and the Constellation authors
 SPDX-License-Identifier: EUPL-1.2
 """
 
+import time
+
+from constellation.core.configuration import Configuration
 from constellation.core.satellite import Satellite
 
 
 class TempMon(Satellite):
-    pass
+    _time_to_throw: float | int
+
+    def do_initializing(self, config: Configuration) -> str:
+        self._time_to_throw = config.setdefault("time_to_throw", 60)
+        return "Initialized"
+
+    def do_run(self, run_identifier: str) -> str:
+        time.sleep(float(self._time_to_throw))
+        msg = "Temperature above critical threshold!"
+        self.log.critical(msg)
+        raise Exception(msg)
+        return ""
